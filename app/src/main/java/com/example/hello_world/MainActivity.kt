7388e86c21a64497559470fbba0318d5ed28c09d
Krzysztof Kopydłowski <28898861+Veith3n@users.noreply.github.com>
2024-03-03 11:15:19 +0100

    feat: Display button click amount on the main view

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.hello_world
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    private var clickCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,14 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val incrementButton: Button = findViewById(R.id.button3)
+        val anotherTextView: TextView = findViewById(R.id.anotherTextView)
+
+        incrementButton.setOnClickListener {
+            clickCount++
+            anotherTextView.text = "Button clicked: $clickCount times"
         }
     }
 }
